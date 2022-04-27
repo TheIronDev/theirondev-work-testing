9214e7050ad38555e72f70dc5225f5edc3d22347
Tyler Stark <theirondeveloper@gmail.com>
2022-04-27 14:58:15 -0500

    Showing current state of source code :wq

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,13 @@
 import {logger} from "firebase-functions";
+import {initializeApp} from "firebase-admin/app";
 
 import {
-  CloudEvent, alerts, pubsub, storage, eventarc,
+  CloudEvent, alerts, pubsub, storage,
+  // eventarc,
 } from "firebase-functions/v2";
+
+initializeApp();
+
 
 const handler = (functionName: string) => {
   return (cloudEvent: CloudEvent) => {
@@ -12,12 +17,11 @@ const handler = (functionName: string) => {
 
 const topic = "tystark-testing-v2-functions-topic";
 const bucket = "tystark-testing-v2-functions";
-const eventType = "eventType";
 
 /* eslint-disable max-len */
-export const onalertpublished = alerts.onAlertPublished("alertType", handler("onAlertPublished"));
+// export const onalertpublished = alerts.onAlertPublished("alertType", handler("onAlertPublished"));
 export const onnewanrissuepublished = alerts.crashlytics.onNewAnrIssuePublished(handler("onNewAnrIssuePublished"));
-export const onnewfatalissuepublished = alerts.crashlytics.onNewFatalIssuePublished(handler("onNewFatalIssuePublished"));
+// export const onnewfatalissuepublished = alerts.crashlytics.onNewFatalIssuePublished(handler("onNewFatalIssuePublished"));
 export const onnewnonfatalissuepublished = alerts.crashlytics.onNewNonfatalIssuePublished(handler("onNewNonfatalIssuePublished"));
 export const onregressionalertpublished = alerts.crashlytics.onRegressionAlertPublished(handler("onRegressionAlertPublished"));
 export const onstabilitydigestpublished = alerts.crashlytics.onStabilityDigestPublished(handler("onStabilityDigestPublished"));
@@ -25,7 +29,7 @@ export const onvelocityalertpublished = alerts.crashlytics.onVelocityAlertPublis
 export const onnewtesteriosdevicepublished = alerts.appDistribution.onNewTesterIosDevicePublished(handler("onNewTesterIosDevicePublished"));
 export const onplanautomatedupdatepublished = alerts.billing.onPlanAutomatedUpdatePublished(handler("onPlanAutomatedUpdatePublished"));
 export const onplanupdatepublished = alerts.billing.onPlanUpdatePublished(handler("onPlanUpdatePublished"));
-export const oncustomeventpublished = eventarc.onCustomEventPublished(eventType, handler("onCustomEventPublished"));
+// export const oncustomeventpublished = eventarc.onCustomEventPublished(eventType, handler("onCustomEventPublished"));
 export const onobjectarchived = storage.onObjectArchived(bucket, handler("onObjectArchived"));
 export const onobjectdeleted = storage.onObjectDeleted(bucket, handler("onObjectDeleted"));
 export const onobjectfinalized = storage.onObjectFinalized(bucket, handler("onObjectFinalized"));
